25b35b045460b66de1b50e9d64a7df5d3ae1e372
kopfmad <santana.agr@gmail.com>
2021-07-23 11:19:47 -0300

    terminei os renderlists

--- src/RenderListDoisElementosInvertido.js
+++ src/RenderListDoisElementosInvertido.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import RenderListDoisElementosInvertidoFilho from './RenderListDoisElementosInvertidoFilho'
 
 export default function RenderListDoisElementosInvertido() {
+  let [minhaArray, setMinhaArray] = useState([
+    'Lorem ipsum dolor sit amet consectetur.',
+    'Lorem ipsum dolor sit amet consectetur.',
+  ])
+
+  let novoitem = 'Lorem ipsum dolor sit amet consectetur. '
+
+  function adicionaNaArray() {
+    setMinhaArray((estadoantigo) => [...estadoantigo, novoitem])
+  }
+
   return (
     <div className="RenderListDoisElementosInvertido">
       <h2>
         Este é o teste de um renderlist, com dois componentes, sendo os outputs
         o pai, e os botões o filho
       </h2>
+
+      {minhaArray.map((item) => (
+        <div>{item}</div>
+      ))}
+      <RenderListDoisElementosInvertidoFilho funcdopai={adicionaNaArray} />
     </div>
   )
 }
---
+++ src/RenderListDoisElementosInvertidoFilho.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function RenderListDoisElementosInvertidoFilho(props) {
+  return (
+    <div className=" RenderListDoisElementosInvertidoFilho">
+      <button className="btn" onClick={props.funcdopai}>
+        + lorem
+      </button>
+    </div>
+  )
+}
