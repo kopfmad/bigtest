855214085881a9a3ceb392fc32c79ca5348520e7
kopfmad <santana.agr@gmail.com>
2021-07-23 10:16:34 -0300

    terminei os inputfields

--- src/InputFieldDoisElementosInvertido.js
+++ src/InputFieldDoisElementosInvertido.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InputFieldDoisElementosInvertidoFilho from './InputFieldDoisElementosInvertidoFilho'
 
 export default function InputFieldDoisElementosInvertido() {
+  let [nome, setNome] = useState('Alexa')
+
+  function getData(valor) {
+    console.log(valor.target.value)
+    setNome(valor.target.value)
+  }
   return (
     <div className="InputFieldDoisElementosInvertido">
       <h2>
         Este é o teste de um inputfield, com dois elementos, sendo o inputfield
         o pai, e o texto o filho
       </h2>
+      <input type="text" placeholder="Digite aqui" onChange={getData} />
+      <InputFieldDoisElementosInvertidoFilho nome={nome} />
     </div>
   )
 }
---
+++ src/InputFieldDoisElementosInvertidoFilho.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function InputFieldDoisElementosInvertidoFilho(props) {
+  return (
+    <div className="InputFieldDoisElementosInvertidoFilho">
+      <h2>O nome é {props.nome}</h2>
+    </div>
+  )
+}
